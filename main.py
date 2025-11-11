32a80ab318a228dc5f03440e3e7e86aa03203b97
leosalgado <leonardosalgado@protonmail.com>
2025-11-10 21:28:19 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from machine import Pin
+import time
+import network
+import urequests
+
+
+def conecta_wifi(ssid, password):
+    print("Connecting to WiFi", end="")
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect(ssid, password)
+    while not sta_if.isconnected():
+        print(".", end="")
+        time.sleep(0.1)
+    print(" Connected!")
+
+
+SSID = "Wokwi-GUEST"
+PASSWORD = ""
+conecta_wifi(SSID, PASSWORD)
+
+PINO_SLIDE_SWITCH = 21
+# Bounce desativado no componente
+switch = Pin(PINO_SLIDE_SWITCH, Pin.IN, Pin.PULL_UP)
+cont = {0: 0}
+last_trigger_time = 0
+
+
+def manda_sinal():
+    try:
+        response = urequests.get("https://httpbin.org/get")
+        print("Sinal enviado!\nResposta:", response.text)
+        response.close()
+    except Exception as e:
+        print("Erro ao enviar sinal:", e)
+
+
+def dispara_alarme(pin):
+    global last_trigger_time
+    current_time = time.ticks_ms()
+
+    if current_time - last_trigger_time > 10000:
+        cont[0] += 1
+        last_trigger_time = current_time
+        print("-" * 45)
+        print(f"  ALERTA: O SWITCH FOI ACIONADO PELA {cont[0]}a VEZ!")
+        print("-" * 45)
+        manda_sinal()
+
+
+switch.irq(trigger=Pin.IRQ_FALLING, handler=dispara_alarme)
+
+print("[SIMULADOR INICIADO]")
+
+while True:
+    time.sleep(1)
